17d3091c1ae133f1142e4678867f175e18668b43
CHI-NONSO1 <chinonso_patrick@yahoo.com>
2022-12-21 02:03:19 +0100

    Create navbar.blade.php

---
+++ backend/resources/views/admin/nav/navbar.blade.php
@@ -0,0 +1,22 @@
+<nav class="navbar">
+
+
+  <div class="navbar-collapse">
+    <ul class="navbar-nav">
+      <form class="form-inline">
+        <input class="form-control" type="text" placeholder="Search">
+        <button class="btn-submit" type="submit">Search</button>
+      </form>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Home</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">About</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Contact</a>
+      </li>
+
+    </ul>
+  </div>
+</nav>
